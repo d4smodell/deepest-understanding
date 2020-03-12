d92bde4752f30772fcb5862a06bb1e374769e86e
Daniil <daniilrealgrad@gmail.com>
2020-03-12 11:30:59 +0300

    Update dialogsReducer.js

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,15 +2,15 @@ const ADD_MESSAGE = 'ADD-MESSAGE'
 
 let initialState = {
     dialogsData: [
-        {id: 1, name: 'Eva'},
-        {id: 2, name: 'Alice'},
-        {id: 3, name: 'Vasilisa'},
-        {id: 4, name: 'Melissa'},
-        {id: 5, name: 'Alexander'},
+        {id: 1, name: 'User 1'},
+        {id: 2, name: 'User 2'},
+        {id: 3, name: 'User 3'},
+        {id: 4, name: 'User 4'},
+        {id: 5, name: 'User 5'},
     ],
     
     messagesData: [
-      {id: 1, message: 'Design decision completes that room very well.'},
+      {id: 1, message: 'Fight Club'},
       {id: 2, message: 'Shutter 2004'},
       {id: 3, message: 'GTO'},
       {id: 4, message: 'Rasen 1998'},
