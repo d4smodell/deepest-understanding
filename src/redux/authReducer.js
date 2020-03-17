6e2f56750ccbcabdcd693e102f162f86c0398d6a
Daniil <daniilrealgrad@gmail.com>
2020-03-17 12:48:48 +0300

    Update authReducer.js

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -26,37 +26,29 @@ const authReducer = (state = initialState, action) => {
 
 export const setUserData = (id, email, login, isAuth) => ({type: SET_USER_DATA, payload: {id, email, login, isAuth}})
 
-export const getData = () => {
-    return (dispatch) => {
-        authAPI.me()
-        .then(data => {
+export const getData = () => async (dispatch) => {
+        let data = await authAPI.me()
             if (data.resultCode === 0) {
                 let {id, email, login} = data.data
                 dispatch(setUserData(id, email, login, true))
             }
-        })
     }
-}
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-    authAPI.login(email, password, rememberMe) 
-    .then(data => {
+export const login = (email, password, rememberMe) => async (dispatch) => {
+    let data = await authAPI.login(email, password, rememberMe) 
         if(data.resultCode === 0) {
             dispatch(getData())
         } else {
             let message = data.messages.length > 0 ? data.messages[0] : 'Some Error'
             dispatch(stopSubmit('login', {_error: message}))
         }
-    })
 }
 
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-    .then(data => {
+export const logout = () => async (dispatch) => {
+    let data = await authAPI.logout()
         if(data.resultCode === 0) {
             dispatch(setUserData(null, null, null, false))
         }
-    })
 }
 
 
