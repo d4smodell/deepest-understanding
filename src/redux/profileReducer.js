d287fb11af779d86deab58fa3e5aa03a332badb8
Daniil <daniilrealgrad@gmail.com>
2020-03-17 12:48:46 +0300

    Update profileReducer.js

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,6 +3,7 @@ import {profileAPI} from './../api/api'
 const ADD_POST = 'ADD-POST'
 const SET_USERS_PROFILE = 'SET-USERS-PROFILE'
 const SET_STATUS = 'SET_STATUS'
+const DELETE_POST = 'DELETE_POST'
 
 let initialState = {
         postData: [
@@ -32,6 +33,9 @@ const profileReducer = (state = initialState, action) => {
         case SET_STATUS:
           return {...state, status: action.status}
 
+        case DELETE_POST:
+          return {...state, postData: state.postData.filter(p => p.id !== action.id)}
+
         default:
             return state
     }
@@ -39,36 +43,24 @@ const profileReducer = (state = initialState, action) => {
 
 export const onAddPost = (newPostText) => ({type: ADD_POST, newPostText})
 
-export const getProfile = (userId) => {
-  return (dispatch) => {
-    profileAPI.getProfile(userId)
-    .then(data => {
+export const getProfile = (userId) => async (dispatch) => {
+    let data = await profileAPI.getProfile(userId)
       dispatch(setUsersProfile(data))
-  })
-  }
 }
 
-export const getStatus = (userId) => {
-  return (dispatch) => {
-    profileAPI.getStatus(userId)
-    .then(data => {
+export const getStatus = (userId) => async (dispatch) => {
+    let data = await profileAPI.getStatus(userId)
       dispatch(setStatus(data))
-    })
   }
-}
 
-export const updateStatus = (status) => {
-  return (dispatch) => {
-    profileAPI.updateStatus(status)
-    .then(data => {
+export const updateStatus = (status) => async (dispatch) => {
+    let data = await profileAPI.updateStatus(status)
       if(data.resultCode === 0) {
         dispatch(setStatus(status))
       }
-    })
   }
-}
 
 export const setUsersProfile = (profile) => ({type: SET_USERS_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
-
+export const deletePost = postId => ({type: DELETE_POST, postId})
 export default profileReducer
