8d4877525949742c1c3c8a1c7c11cf5352ce663c
Daniil <daniilrealgrad@gmail.com>
2020-03-19 12:56:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import HeaderContainer from './Components/Header/HeaderContainer'
 import Navbar from './Components/Navbar/Navbar'
 import store from './redux/redux-store'
-import ProfileContainer from './Components/Profile/ProfileContainer'
-import DialogsContainer from './Components/Dialogs/DialogsContainer'
 import s from './Components/Users/Users.module.css'
 import Music from './Components/Music/Music'
 import News from './Components/News/News'
 import Settings from './Components/Settings/Settings'
-import UsersContainer from './Components/Users/UsersContainer'
 import {BrowserRouter, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import Login from './Components/Login/Login'
 import './App.css';
 import { withRouter } from 'react-router-dom'
 import { getData } from './redux/authReducer'
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { initializeApp } from './redux/appReducer'
+
+const Login = React.lazy(() => import('./Components/Login/Login'))
+const UsersContainer = React.lazy(() => import('./Components/Users/UsersContainer'))
+const ProfileContainer = React.lazy(() => import('./Components/Profile/ProfileContainer')) 
+const DialogsContainer = React.lazy(() => import('./Components/Dialogs/DialogsContainer'))
 
 class App extends React.Component {
   componentDidMount() {
@@ -33,6 +34,7 @@ class App extends React.Component {
         <HeaderContainer/>
         <Navbar/>
         <div className='appWrapperContent'>
+        <Suspense fallback={<img className={s.toggleLoader} src='https://брайтфит.рф/style/images/i-loader.png' alt='load' />}>
         <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
         <Route path='/dialogs' render={() => <DialogsContainer />}/>
         <Route path='/news' render={() => <News/>}/>
@@ -40,6 +42,7 @@ class App extends React.Component {
         <Route path='/settings' render={() => <Settings/>}/>
         <Route path='/users' render={() => <UsersContainer/>}/>
         <Route path='/login' render={() => <Login/>}/>
+        </Suspense>
         </div>
       </div>
       </BrowserRouter>
